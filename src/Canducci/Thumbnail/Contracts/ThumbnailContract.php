1becf727a15986053f1ccf911215a2368b32a5cf
netdragoon <netdragoon@outlook.com>
2015-12-03 17:50:50 -0200

    thumb beta3

--- src/Canducci/Thumbnail/Contracts/ThumbnailContract.php
+++ src/Canducci/Thumbnail/Contracts/ThumbnailContract.php
@@ -40,13 +40,14 @@ abstract class ThumbnailContract
     }
     protected  function renderThumbnailTagVideoEmbed($width = 560, $height = 315, $frameborder = 0, $suggestvideo = true, $controls = true, $showinfo = true, $privacidade = false)
     {
-        $url = $privacidade ? ThumbnailUrl::URLEmbedPrivacidade : ThumbnailUrl::URLEmbed;
-        $conf = $suggestvideo === false ? "rel=0" : "";
-        if ($conf !=="" && !$controls) $conf += "&amp;";
-        $conf += !$controls ? "controls=0" : "";
-        if ($conf !=="" && !$showinfo) $conf += "&amp;";
-        $conf += !$showinfo ? "showinfo=0" : "";
-        if ($conf !=="") $conf = "?" + $conf;
+        $url = ($privacidade) ? ThumbnailUrl::URLEmbedPrivacidade : ThumbnailUrl::URLEmbed;
+
+        $conf = ($suggestvideo == false) ? "rel=0" : "";
+        if ($conf !=="" && $controls == false) $conf .= "&amp;&";
+        $conf .= ($controls == false) ? "controls=0" : "";
+        if ($conf !=="" && $showinfo == false) $conf .= "&amp;&";
+        $conf .= ($showinfo == false) ? "showinfo=0" : "";
+        if ($conf !=="") $conf = "?" . $conf;
 
         $embed = sprintf('<iframe width="%s" height="%s" ', $width, $height);
         $embed .= sprintf('src="%s%s" ', sprintf($url, $this->code), $conf);
@@ -59,7 +60,8 @@ abstract class ThumbnailContract
         $parseUrl = parse_url($this->url);
         $response = array();
         preg_match('/v=[\w+]*/', $parseUrl['query'], $response);
-        if (count($response) == 1){
+        if (count($response) == 1)
+        {
             $this->code = str_replace("v=", "",$response[0]);
             return true;
         }
